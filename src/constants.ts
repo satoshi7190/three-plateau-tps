7a665e25cf6cfcb35810f7106b9a4166e4d892db
satoshi komatsu <satoshi7190@gmail.com>
2024-12-19 07:54:24 +0900

    push

--- src/constants.ts
+++
@@ -1,3 +0,0 @@
-export const SCENE_CENTER_COORDS: [number, number] = [-12043, -34145]; // シーンの中心にする地理座標[x, y] (EPSG:6677)
-export const INITIAL_LNG_LAT: [number, number] = [139.699361, 35.692191]; // シーンの初期緯度
-export const INITIAL_MODEL_ROTATION: number = 90; // シーンの初期ズームレベル
